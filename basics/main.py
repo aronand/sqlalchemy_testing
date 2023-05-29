f1267bf97d3c061afc93f4dbfb3bce3afc8f944c
aronand <71503746+aronand@users.noreply.github.com>
2023-05-29 22:34:03 +0300

    Moves functionality within a Program class

--- basics/main.py
+++ basics/main.py
@@ -5,15 +5,19 @@ from sqlalchemy import create_engine, Engine
 import models
 
 
-def main() -> None:
-    db_file = Path.cwd() / "test.db"
-    engine = create_engine(f"sqlite:///{db_file.name}", echo=True)
-    db_file.exists() or create_database(engine)  # equivalent to if not exists [...]
+class Program:
+    def __init__(self):
+        self.db_file = Path.cwd() / "test.db"
+        self.engine: None | Engine = None
 
+    def startup(self) -> None:
+        self.engine = create_engine(f"sqlite:///{self.db_file.name}", echo=True)
+        self.db_file.exists() or self.create_database()  # equivalent to if not exists [...]
 
-def create_database(engine: Engine) -> None:
-    models.Base.metadata.create_all(engine)
+    def create_database(self) -> None:
+        models.Base.metadata.create_all(self.engine)
 
 
 if __name__ == "__main__":
-    main()
+    p = Program()
+    p.startup()
